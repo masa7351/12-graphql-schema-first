b5806097bec2f8b92f9e3fece3978a8086328f89
masa7351 <masa7351@gmail.com>
2022-09-06 23:54:37 +0900

    Redisキャッシュ用のパッケージ追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,32 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { CatsModule } from "./cats/cats.module";
-import { upperDirectiveTransformer } from "./common/directives/upper-case.directive";
+// import { upperDirectiveTransformer } from "./common/directives/upper-case.directive";
 import { RolesModule } from "./roles/roles.module";
-import {ApolloServerPluginCacheControl} from 'apollo-server-core'
-import responseCachePlugin from 'apollo-server-plugin-response-cache'
+import { ApolloServerPluginCacheControl } from "apollo-server-core";
+import responseCachePlugin from "apollo-server-plugin-response-cache";
 
 @Module({
   imports: [
     CatsModule,
     RolesModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ["./**/*.graphql"],
-      // transformSchema: (schema) => upperDirectiveTransformer(schema, "upper"),
-      // installSubscriptionHandlers: true,
-      plugins: [
-        ApolloServerPluginCacheControl({
-          defaultMaxAge: 50,
-          calculateHttpHeaders: false,
-        }),
-        responseCachePlugin(),
-      ],
+      useFactory: async () => {
+        const options = {
+          typePaths: ["./**/*.graphql"],
+          // transformSchema: (schema) => upperDirectiveTransformer(schema, "upper"),
+          // installSubscriptionHandlers: true,
+          plugins: [
+            ApolloServerPluginCacheControl({
+              defaultMaxAge: 50,
+              calculateHttpHeaders: false,
+            }),
+            responseCachePlugin(),
+          ],
+        };
+        return options;
+      },
     }),
   ],
 })
