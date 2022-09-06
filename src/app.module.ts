33d1fe9aade374d5a522df4239d03c83d2030a11
masa7351 <masa7351@gmail.com>
2022-09-07 07:07:04 +0900

    キャッシュを有効化していると起動しなくなるので一旦無効化

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { GraphQLModule } from "@nestjs/graphql";
 import { CatsModule } from "./cats/cats.module";
 // import { upperDirectiveTransformer } from "./common/directives/upper-case.directive";
 import { RolesModule } from "./roles/roles.module";
-import { ApolloServerPluginCacheControl } from "apollo-server-core";
-import responseCachePlugin from "apollo-server-plugin-response-cache";
-
+import {
+  ApolloServerPluginCacheControl,
+  // ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+// import responseCachePlugin from "apollo-server-plugin-response-cache";
+// import Keyv from "keyv";
+// import KeyvRedis from "@keyv/redis";
+// import Redis from "ioredis";
+// import { KeyvAdapter } from "@apollo/utils.keyvadapter";
 @Module({
   imports: [
     CatsModule,
@@ -14,6 +20,7 @@ import responseCachePlugin from "apollo-server-plugin-response-cache";
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       useFactory: async () => {
+        // const cluster = new Redis.Cluster([{ host: "localhost", port: 6379 }]);
         const options = {
           typePaths: ["./**/*.graphql"],
           // transformSchema: (schema) => upperDirectiveTransformer(schema, "upper"),
@@ -23,8 +30,12 @@ import responseCachePlugin from "apollo-server-plugin-response-cache";
               defaultMaxAge: 50,
               calculateHttpHeaders: false,
             }),
-            responseCachePlugin(),
+            // responseCachePlugin(),
+            // ApolloServerPluginLandingPageLocalDefault({ embed: true }),
           ],
+          // cache: new KeyvAdapter(new Keyv({ store: new KeyvRedis(cluster) }), {
+          //   disableBatchReads: true,
+          // }),
         };
         return options;
       },
